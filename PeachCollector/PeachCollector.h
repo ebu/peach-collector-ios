dad9f4b50bb5b18dcbc4979a1d33709e0cae83f0
Rayan Arnaout <arnaout@ebu.ch>
2019-09-24 11:13:35 +0200

    Initial Commit

---
+++ PeachCollector/PeachCollector.h
@@ -0,0 +1,19 @@
+//
+//  PeachCollector.h
+//  PeachCollector
+//
+//  Created by Rayan Arnaout on 24.09.19.
+//  Copyright © 2019 European Broadcasting Union. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PeachCollector.
+FOUNDATION_EXPORT double PeachCollectorVersionNumber;
+
+//! Project version string for PeachCollector.
+FOUNDATION_EXPORT const unsigned char PeachCollectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PeachCollector/PublicHeader.h>
+
+
---
+++ PeachCollectorTests/PeachCollectorTests.m
@@ -0,0 +1,37 @@
+//
+//  PeachCollectorTests.m
+//  PeachCollectorTests
+//
+//  Created by Rayan Arnaout on 24.09.19.
+//  Copyright © 2019 European Broadcasting Union. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PeachCollectorTests : XCTestCase
+
+@end
+
+@implementation PeachCollectorTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
