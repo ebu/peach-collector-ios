d917d9a4fd2fcae6d166a2239faf95226145b3c4
Rayan Arnaout <arnaout@ebu.ch>
2020-08-28 08:51:07 +0200

    Added TvOS demo target

---
+++ PeachCollectorTVDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PeachCollectorTVDemo
+//
+//  Created by Rayan Arnaout on 18.08.20.
+//  Copyright © 2020 European Broadcasting Union. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PeachCollectorTVDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PeachCollectorTVDemo
+//
+//  Created by Rayan Arnaout on 18.08.20.
+//  Copyright © 2020 European Broadcasting Union. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
