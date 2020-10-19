6f85a2bb97139d187863c4170802d54d432a877d
Rayan Arnaout <arnaout@ebu.ch>
2020-10-19 14:58:32 +0200

    Updated Persistant Container to fetch model file in case of SPM compilation

--- Sources/PeachCollector/PeachPersistentContainer.m
+++ Sources/PeachCollector/PeachPersistentContainer.m
@@ -10,9 +10,31 @@
 
 @implementation PeachPersistentContainer
 
+- (instancetype)initWithName:(NSString *)name{
+#if SWIFT_PACKAGE
+    if (SWIFTPM_MODULE_BUNDLE) {
+        NSBundle* bundle = SWIFTPM_MODULE_BUNDLE;
+        NSLog(@"bundle2 : %@", bundle.bundlePath);
+        NSURL * modelURL = [bundle URLForResource:@"PeachCollector" withExtension:@"mom"];
+        NSManagedObjectModel * model = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
+        return [super initWithName:name managedObjectModel:model];
+    }
+#endif
+    return [super initWithName:name];
+}
+
++ (NSBundle *)getBundle {
+#if SWIFT_PACKAGE
+  return SWIFTPM_MODULE_BUNDLE;
+#else
+  return [NSBundle bundleForClass:[PeachPersistentContainer class]];
+#endif
+}
+
 + (NSURL *)defaultDirectoryURL
 {
     return [[super defaultDirectoryURL] URLByAppendingPathComponent:@"PeachCollector"];
 }
+
 
 @end
